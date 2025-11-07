eeecbb45cc4f65cbd0351f0276ee7a07762d0d14
Neeraali-digital <neeraalidigitalweb@gmail.com>
2025-11-07 18:58:02 +0530

    review integration

--- reviews/models.py
+++ reviews/models.py
@@ -11,6 +11,7 @@ class Review(models.Model):
     company = models.CharField(max_length=100)
     rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
     review = models.TextField()
+    image = models.ImageField(upload_to='reviews/', blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
 
--- reviews/serializers.py
+++ reviews/serializers.py
@@ -2,16 +2,22 @@ from rest_framework import serializers
 from .models import Review
 
 class ReviewSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(required=False)
+
     class Meta:
         model = Review
         fields = '__all__'
 
 class ReviewListSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(required=False)
+
     class Meta:
         model = Review
-        fields = ['id', 'name', 'company', 'rating', 'review', 'date']
+        fields = ['id', 'name', 'company', 'rating', 'review', 'image', 'date']
 
 class ReviewCreateSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(required=False)
+
     class Meta:
         model = Review
-        fields = ['name', 'company', 'rating', 'review']
+        fields = ['name', 'company', 'rating', 'review', 'image']
